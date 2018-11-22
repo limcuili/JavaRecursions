a83f06cc879bef8abf521264d7e5ef3dabab3e19
Cui Li LIM <38138998+limcuili@users.noreply.github.com>
2018-11-22 10:42:24 +0000

    fixed fibonacci main method params

--- Fibonacci.java
+++ Fibonacci.java
@@ -8,7 +8,7 @@ class Fibonacci {
         return fibonacci(n-1) + fibonacci(n-2);
     }
 
-    public static void main (String args[]) {
+    public static void main (String[] args) {
         int n = Integer.parseInt(args[0]);
         System.out.println(fibonacci(n));
     }
