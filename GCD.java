85e154742b57011df8c8ae29606a1a0108a0d048
Cui Li LIM <38138998+limcuili@users.noreply.github.com>
2018-11-22 10:42:41 +0000

    fixed gcd main method params

--- GCD.java
+++ GCD.java
@@ -24,7 +24,7 @@ class GCD {
     */
 
 
-    public static void main (String args[]) {
+    public static void main (String[] args) {
         int x = Integer.parseInt(args[0]);
         int y = Integer.parseInt(args[1]);
         System.out.println(gcd(x,y));
